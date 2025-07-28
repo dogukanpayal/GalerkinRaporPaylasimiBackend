41e3db927b976658a6dae8e2b6cf78c488a4e55f
dogukanpayal <payaldogukan@gmail.com>
2025-07-28 14:09:18 +0300

    Publish 2

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -8,13 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mssql',
-    dialectOptions: {
-      options: {
-        encrypt: true,
-        trustServerCertificate: true,
-      },
-    },
+    dialect: 'postgres',
     logging: false,
   }
 );
